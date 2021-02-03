c8383077924083695a1ba1cddb5cb4017741bde6
lilian cansell <lilian.cansell.dev@gmail.com>
2021-02-03 15:20:52 +0100

    premier commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
+ 
+//@Component({
+  //selector: 'app-root',
+  //templateUrl:'./app.component.html'
+//})
+//export class AppComponent implements OnInit {
+ 
+  //pokemons: Pokemon[] = null;
+ 
+  //ngOnInit() {
+    //this.pokemons = POKEMONS;
+  //}
+ 
+  //selectPokemon(pokemon: Pokemon) {
+    //console.log('Vous avez selectionné ' + pokemon.name);
+  //}
+   
+//}
